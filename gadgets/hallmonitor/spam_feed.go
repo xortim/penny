cfa89bde0c80b6bee998084c94f4a5fc92f4e266
Devin Roark <roarkd@gmail.com>
2022-07-17 17:40:12 -0400

    Replace mysteriously not functioning UID with slack username
    also wrap the user check in a query in case we need it elsewhere

--- gadgets/hallmonitor/spam_feed.go
+++ gadgets/hallmonitor/spam_feed.go
@@ -257,3 +257,9 @@ func addDebugResponse(removed bool, score int, reasons []string, msg slack.Messa
 	}
 	return err
 }
+
+func getUserInfo(uid string, api *slack.Client) (*slack.User, error) {
+	user, err := api.GetUserInfo(uid)
+
+	return user, err
+}
